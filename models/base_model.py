9f158d22b76bc675609d13dd0b7d6f6485a1ce02
Azi-C <aaziza.chbihi@gmail.com>
2024-02-05 15:07:12 -0800

    adding models

---
+++ models/base_model.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+'''
+Defines BaseModel Class
+'''
+import cmd
+import json
+import uuid
+import models
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Represents the BaseModel class for Airbnb clone project
+    Methods:
+        __init__(self, *args, **kwargs)
+        __str__(self)
+        __save(self)
+        to_dict(self)
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes new instance of the BaseModel class
+        """
+        form = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                    self.__dict__[key] = datetime.strptime(value, form)
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """
+        Returns string representation of BaseModel
+        """
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id,
+                                      self.__dict__))
+
+    def __repr__(self):
+        """
+        Returns string representation
+        """
+        return (self.__str__())
+
+    def save(self):
+        """
+        Updates the attribute with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Converts the BaseModel instance to a dictionary representation
+        """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
